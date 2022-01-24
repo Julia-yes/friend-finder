3f19422e013286f238fd890b42b572708fdf96df
Julia-yes <87761523+Julia-yes@users.noreply.github.com>
2022-01-24 18:25:16 +0300

    comm.2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { values } from "redux-form";
+
+const ErrorPattern = (item, minSumbols, maxSumbols) => (values) => {
+item : Yup.string()
+  .min(minSumbols, 'Too Short!')
+  .max(maxSumbols, 'Too Long!')
+  .required('Required')
+}
+
+const ErrorMessagesSchema = Yup.object().shape({
+  ErrorPattern(login, 2, 50),
+  ErrorPattern(password, 2, 50),
+});
 
 const Login = (props) => {
     return <div>
